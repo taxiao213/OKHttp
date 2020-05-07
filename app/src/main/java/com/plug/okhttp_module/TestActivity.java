9eeb267b4467a5c403dd95e1ee3455079ab05d24
taxiao213 <han13593427958>
2020-05-07 22:44:29 +0800

    handler写法

--- app/src/main/java/com/plug/okhttp_module/TestActivity.java
+++ app/src/main/java/com/plug/okhttp_module/TestActivity.java
@@ -1,6 +1,8 @@
 package com.plug.okhttp_module;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,5 +17,30 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
+
+        // handleMessage 回调方法
+        new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
+        // Handler.Callback handleMessage 回调方法
+        new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                return false;
+            }
+        });
+
+        // message 的 callback
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
     }
 }
